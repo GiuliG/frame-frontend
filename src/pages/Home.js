1340c52529c693dedd781c3cb53e577a2fee8aa1
ceciliabarudi <blasterkat101@gmail.com>
2018-12-15 21:20:50 +0100

    clean link routes to exhibition list, working list, extract card components- to do - fix props

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,10 @@ class Home extends Component {
       <div>
         <h3>Check out exhibitions</h3>
         <ul>
-          <li><Link to={'me/paintings/asian-art'}>Asian Art</Link></li>
-          <li><Link to={'me/paintings/european-paintings'}>European Paintings</Link></li>
-          <li><Link to={'me/paintings/modern-contemporary'}>Modern And Contemporary Art </Link></li>
-          <li><Link to={'me/paintings/the-american-wing'}>The American Wing</Link></li>
+          <li><Link to={'me/paintings/asian'}>Asian Art</Link></li>
+          <li><Link to={'me/paintings/european'}>European Paintings</Link></li>
+          <li><Link to={'me/paintings/modern'}>Modern And Contemporary Art </Link></li>
+          <li><Link to={'me/paintings/american'}>The American Wing</Link></li>
         </ul>
       </div>
     );
