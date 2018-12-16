5af5e720f0313fee8cca76fb9e00602fe1f1745c
ceciliabarudi <blasterkat101@gmail.com>
2018-12-16 21:13:17 +0100

    make navbar semantic, styling by percentage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,25 +6,45 @@ import { withAuth } from '../providers/AuthProvider';
 class Navbar extends Component {
 
   renderIsLoggedIn = () => {
-    return <div className="logged-in">
-      <Link to='/'><img src={process.env.PUBLIC_URL + '/icons/cam-shutter-solid.svg'} alt="scan a painting" /></Link>
-      <Link to='/'><img src={process.env.PUBLIC_URL + '/icons/home-solid.svg'} alt="to homepage" className="homeicon"/></Link>
-      <Link to='/private'><img src={process.env.PUBLIC_URL + '/icons/user-circle-solid.svg'} alt={this.props.user.username} /></Link>
-    </div>
+    return <ul className="logged-in">
+      <li>
+        <Link to='/'>
+          <img src={process.env.PUBLIC_URL + '/icons/cam-shutter-solid.svg'} alt="scan a painting" />
+        </Link>
+      </li>
+      <li>
+        <Link to='/'>
+          <img src={process.env.PUBLIC_URL + '/icons/home-solid.svg'} alt="to homepage" className="homeicon" />
+        </Link>
+      </li>
+      <li>
+        <Link to='/private'>
+          <img src={process.env.PUBLIC_URL + '/icons/user-circle-solid.svg'} alt={this.props.user.username} />
+        </Link>
+      </li>
+    </ul>
   }
 
   renderIsNotLoggedIn = () => {
-    return <div className="logged-out">
-      <Link to='/signup'><img src={process.env.PUBLIC_URL + '/icons/user-plus-solid.svg'} alt="sign up" className="signupicon"/></Link>
-      <Link to='/login'><img src={process.env.PUBLIC_URL + '/icons/sign-in-alt-solid.svg'} alt="log in" /></Link>
-    </div>
+    return <ul className="logged-out">
+      <li>
+        <Link to='/signup'>
+          <img src={process.env.PUBLIC_URL + '/icons/user-plus-solid.svg'} alt="sign up" className="signupicon" />
+        </Link>
+      </li>
+      <li>
+        <Link to='/login'>
+          <img src={process.env.PUBLIC_URL + '/icons/sign-in-alt-solid.svg'} alt="log in" />
+        </Link>
+      </li>
+    </ul>
   }
 
   render() {
     return (
-      <div id="navbar">
-        { this.props.isLogged ? this.renderIsLoggedIn() : this.renderIsNotLoggedIn() }
-      </div>
+      <nav id="navbar">
+        {this.props.isLogged ? this.renderIsLoggedIn() : this.renderIsNotLoggedIn()}
+      </nav>
     )
   }
 }
