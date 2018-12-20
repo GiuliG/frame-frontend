9d1ca5d8b5304a26481bc58dc28b360ce25f21a9
MarloMorgan <granatogi@gmail.com>
2018-12-20 10:53:00 +0100

    scan is working

--- src/components/ScanForm.js
+++ src/components/ScanForm.js
@@ -1,39 +1,98 @@
 import React, { Component } from 'react';
 import { withAuth } from '../providers/AuthProvider';
-import axios from 'axios';
 import scanService from '../lib/scan-service';
 import FileBase64 from 'react-file-base64';
+import PaintingCard from './PaintingCard';
+
+
 
 
 class ScanForm extends Component {
-   state = {
-    files: []
-   }
-    fileSelctedHandler = (event) =>{
+    state = {
+        file: [],
+        title: '',
+        isLoading: true,
+        description: [],
+    }
+    fileSelctedHandler = (event) => {
         console.log(event)
-        this.setState = {
-            selectedFile: event.target.files[0],
-        }
+        // this.setState = {
+        //     file: ,
+        // }
     }
 
-    //fileUploadHandlers = (event) => {
-        //console.log(event)
-        //const formData = new FormData()
-        //formData.append('myFile', this.state.selectedFile, this.state.selectedFile.name)
-    //}
+    fileUploadHandlers = () => {
+        const string = this.state.file;
+        scanService.getTextFromImage(string)
+            .then((title) => {
+                this.setState({
+                    title,
+                    isLoading: false,
+                })  
+            })
+            .then(() =>{
+                this.getDescription();
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
 
-     // Callback~
-     getFiles = (files)=> {
-        this.setState({ files: files })
-      }
+    // renderCard = () =>{
+    //     const painting = this.state.description[0];
+    //     console.log('called render card')
+    //     console.log(painting)
+    //     return <PaintingCard image={painting.image}
+    //     artist={painting.artist} date={painting.date}
+    //     department={painting.department} description={painting.description}
+    //     title={painting.title} id={painting._id}/>
+    // }
 
-render(){
-    return( <div>
-           {/* <input type="file" onClick={this.fileSelctedHandler}/>*/}
-                <button onClick={this.fileSelctedHandler}>Submit</button>
-                <FileBase64
-                    multiple={ true }
-                    onDone={ this.getFiles} />
+    // Callback~
+    getFiles = (file) => {
+        console.log(file)
+        this.setState(
+            { file: file[0].base64 })
+    }
+
+    //get description from our database
+    getDescription = () => {
+        const { title } = this.state
+        scanService.getPaintingFromTitleScan({ title })
+            .then((result) => {
+                //console.log(result, 'hey')
+                this.setState({
+                    description: result,
+                })
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    render() {
+        const isLoadingNow = this.state.isLoading;
+        console.log(this.state.description)
+
+        return (<div>
+            {/* <input type="file" onClick={this.fileSelctedHandler}/>*/}
+            <button onClick={this.fileUploadHandlers}>Submit</button>
+            <FileBase64 multiple={true} onDone={this.getFiles} />
+            {isLoadingNow ? <p>Processing your image...</p> : <div> 
+                {(this.state.description.length) ? <div className="painting-card">
+                    <img src={this.state.description[0].image} alt={this.state.description[0].title} />
+                    <h2>{this.state.description[0].title}</h2>
+                    <h5>{this.state.description[0].artist} - {this.state.description[0].date}</h5>
+                    <p>{this.state.description[0].description}</p>
+                    <p>Listen to Audio Guide:</p>
+                    <audio controls src={this.state.description[0].audio} type="audio/mp3">
+                    
+                    </audio>
+                    {/*<HeartButton paintingId={this.props.id} isFavorite={this.props.isFavorite} deleteFav={this.props.deleteFav}*/}
+                </div> : <p>Not part of the MET collection</p>}
+            </div>}
+
+
         </div>
         )
     }
@@ -42,4 +101,3 @@ render(){
 export default ScanForm
 
 
-  
--- src/lib/scan-service.js
+++ src/lib/scan-service.js
@@ -1,48 +1,60 @@
 
 import axios from 'axios';
-import FileBase64 from 'react-file-base64';
+const frameApiKey = '';
 
 class ScanApi {
-  constructor() {
-    this.apiInstance = axios.create({
-      baseURL: `${process.env.REACT_APP_API_URL}`,
-      withCredentials: true
-    });
-  
+    constructor() {
+        this.apiInstance = axios.create({
+            baseURL: `${process.env.REACT_APP_API_URL}`,
+            withCredentials: true
+        });
 
-  // set up Google Cloud Vision API
-  this.googleVision = axios.create({
-    baseURL: `https://vision.googleapis.com/v1`
-  });
-}
 
-getTextFromImage(imageBase64String) {
-    imageBase64String = imageBase64String.substring(22) // trim 'data:image/png;base64,' from the Base64 string
-    console.log(imageBase64String);
-    
-    const requestBody = {
-      "requests" : [
-        {
-          "image": 
-            {
-              "content": imageBase64String
-            },
-          "features": [
-            {
-              "type": "LOGO_DETECTION"
-            }
-          ]
-        }
-      ]
+        // set up Google Cloud Vision API
+        this.googleVision = axios.create({
+            baseURL: `https://vision.googleapis.com/v1`
+        });
     }
 
+    getTextFromImage(imageBase64String) {
 
-// request to Google Cloud Vision api
-return this.googleVision.post('/images:annotate?key=AIzaSyBGhAh3nMZ8PgFGCPMtVaDKaLs7JyzyK8Y', requestBody)
-.then((response) => {
-  console.log(response);
-  return response.data.responses[0].textAnnotations
-        });    
+        imageBase64String = imageBase64String.substring(23) // trim 'data:image/png;base64,' from the Base64 string
+        console.log(imageBase64String);
+
+        const requestBody = {
+            "requests": [
+                {
+                    "image":
+                    {
+                        "content": `${imageBase64String}`,
+                    },
+                    "features": [
+                        {
+                            "type": "LOGO_DETECTION",
+                            "maxResults": 1,
+                        }
+                    ]
+                }
+            ]
+        }
+
+
+
+        // request to Google Cloud Vision api
+        return this.googleVision.post("/images:annotate?key=", requestBody)
+            .then((response) => {
+                console.log(response.data.responses[0].logoAnnotations[0].description);
+                return response.data.responses[0].logoAnnotations[0].description
+            });
+    }
+
+    getPaintingFromTitleScan(paintingTitle) {
+        const { title } = paintingTitle
+        return this.apiInstance.get('/api/paintings/scan', {params: {title: title} })
+            .then((response) => {
+                console.log(response)
+                return response.data
+            })
     }
 }
 
