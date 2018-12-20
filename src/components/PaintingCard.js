70a51d3a3585a1801a25239a7cbc875073a0d285
ceciliabarudi <blasterkat101@gmail.com>
2018-12-20 11:35:11 +0100

    cleanup code a bit, indentation

--- src/components/PaintingCard.js
+++ src/components/PaintingCard.js
@@ -7,19 +7,13 @@ import { Link } from 'react-router-dom';
 
 class PaintingCard extends Component {
 
-  click() {
-    console.log("the button was clicked")
-  }
-
   render() {
     return (
       <div className="painting-card">
         <img src={this.props.image} alt={this.props.title} />
         <h2>{this.props.title}</h2>
         <h5>{this.props.artist} - {this.props.date}</h5>
-       
         <Link to={`/me/paintings/${this.props.id}`}> Explore</Link>
-        {/*<p>{this.props.description}</p>*/}
         <HeartButton paintingId={this.props.id} isFavorite={this.props.isFavorite} deleteFav={this.props.deleteFav}/>
       </div>
     );
--- src/components/ScanForm.js
+++ src/components/ScanForm.js
@@ -2,96 +2,74 @@ import React, { Component } from 'react';
 import scanService from '../lib/scan-service';
 import FileBase64 from 'react-file-base64';
 
-
 class ScanForm extends Component {
-    state = {
-        file: [],
-        title: '',
-        isLoading: true,
-        description: [],
-    }
-    fileSelctedHandler = (event) => {
-        console.log(event)
-        // this.setState = {
-        //     file: ,
-        // }
-    }
+  state = {
+    file: [],
+    title: '',
+    isLoading: true,
+    description: [],
+  }
 
-    fileUploadHandlers = () => {
-        const string = this.state.file;
-        scanService.getTextFromImage(string)
-            .then((title) => {
-                this.setState({
-                    title,
-                    isLoading: false,
-                })  
-            })
-            .then(() =>{
-                this.getDescription();
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }
+  fileUploadHandlers = () => {
+    const string = this.state.file;
+    scanService.getTextFromImage(string)
+      .then((title) => {
+        this.setState({
+          title,
+          isLoading: false,
+        })
+      })
+      .then(() => {
+        this.getDescription();
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 
-    // renderCard = () =>{
-    //     const painting = this.state.description[0];
-    //     console.log('called render card')
-    //     console.log(painting)
-    //     return <PaintingCard image={painting.image}
-    //     artist={painting.artist} date={painting.date}
-    //     department={painting.department} description={painting.description}
-    //     title={painting.title} id={painting._id}/>
-    // }
+  // Callback~
+  getFiles = (file) => {
+    this.setState(
+      { file: file[0].base64 })
+  }
 
-    // Callback~
-    getFiles = (file) => {
-        console.log(file)
-        this.setState(
-            { file: file[0].base64 })
-    }
+  //get description from our database
+  getDescription = () => {
+    const { title } = this.state
+    scanService.getPaintingFromTitleScan({ title })
+      .then((result) => {
+        this.setState({
+          description: result,
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 
-    //get description from our database
-    getDescription = () => {
-        const { title } = this.state
-        scanService.getPaintingFromTitleScan({ title })
-            .then((result) => {
-                //console.log(result, 'hey')
-                this.setState({
-                    description: result,
-                })
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }
+  render() {
+    const isLoadingNow = this.state.isLoading;
+    return (<div>
+      <button onClick={this.fileUploadHandlers}>Submit</button>
+      <FileBase64 multiple={true} onDone={this.getFiles} />
+      {isLoadingNow ? <p>Processing your image...</p> : <div>
+        {(this.state.description.length) ? <div className="painting-card">
+          <img src={this.state.description[0].image} alt={this.state.description[0].title} />
+          <h2>{this.state.description[0].title}</h2>
+          <h5>{this.state.description[0].artist} - {this.state.description[0].date}</h5>
+          <p>{this.state.description[0].description}</p>
+          <p>Listen to Audio Guide:</p>
+          <audio controls src={this.state.description[0].audio} type="audio/mp3">
 
-    render() {
-        const isLoadingNow = this.state.isLoading;
-        console.log(this.state.description)
-
-        return (<div>
-            {/* <input type="file" onClick={this.fileSelctedHandler}/>*/}
-            <button onClick={this.fileUploadHandlers}>Submit</button>
-            <FileBase64 multiple={true} onDone={this.getFiles} />
-            {isLoadingNow ? <p>Processing your image...</p> : <div> 
-                {(this.state.description.length) ? <div className="painting-card">
-                    <img src={this.state.description[0].image} alt={this.state.description[0].title} />
-                    <h2>{this.state.description[0].title}</h2>
-                    <h5>{this.state.description[0].artist} - {this.state.description[0].date}</h5>
-                    <p>{this.state.description[0].description}</p>
-                    <p>Listen to Audio Guide:</p>
-                    <audio controls src={this.state.description[0].audio} type="audio/mp3">
-                    
-                    </audio>
-                    {/*<HeartButton paintingId={this.props.id} isFavorite={this.props.isFavorite} deleteFav={this.props.deleteFav}*/}
-                </div> : <p>Not part of the MET collection</p>}
-            </div>}
+          </audio>
+          {/*<HeartButton paintingId={this.props.id} isFavorite={this.props.isFavorite} deleteFav={this.props.deleteFav}*/}
+        </div> : <p>Not part of the MET collection</p>}
+      </div>}
 
 
-        </div>
-        )
-    }
+    </div>
+    )
+  }
 }
 
 export default ScanForm
